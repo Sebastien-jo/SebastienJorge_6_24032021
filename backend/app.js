c376d95a8bfe7fe3a26e5741c47b9910a56f787b
Sebastien Jorge <jorgesebastien@gmail.com>
2021-03-24 17:56:35 +0100

    création des api

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+const path = require('path');
+
+mongoose.connect('mongodb+srv://seb_j:CarotteNavetPoireauxLapin@pekocko.ymnzh.mongodb.net/pekocko?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à Mongodb réussie !'))
+    .catch(() => console.log('Connexion à Mongodb échouée !'));
+
+    const app = express();
+
+app.use((req, res, next)=>{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauce', sauceRoutes);
+app.use('/api/auth', userRoutes);
+module.exports = app;
